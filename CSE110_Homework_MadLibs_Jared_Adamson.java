1230b4d6ff54464f9aa062f9bce7b1dbab756ca5
Jared Adamson <jared.d.adamson@gmail.com>
2014-10-29 21:42:54 -0700

    Update CSE110_Homework_MadLibs_Jared_Adamson.java

--- CSE110_Homework_MadLibs_Jared_Adamson.java
+++ CSE110_Homework_MadLibs_Jared_Adamson.java
@@ -1,16 +1,6 @@
 //Jared Adamson
 
-/* 1. [2 Points] Briefly explain the problem solving and program development process.
 
-First there needs to be a problem to solve then ideas are generated to solve the issue.  a general outline is made and sudo code.  Then the program is made.  From there testing and refining is done until your have a working solution.
-
-
-
-2.  [2 Points] Why do we need to compile java programs before executing them? Clearly explain your answer.
-
-Java is a high level language is must be broken down into a machine level language so the computer or virtual machine can understand and exicute the instructions.  This is what the compiler does.
-
-  */
 
   public class MadLibs
   {
