c1e6c9b1175c649d3de3c3b9ec00cff896948ce7
Jared Adamson <jared.d.adamson@gmail.com>
2014-10-29 21:54:51 -0700

    Create HW3.java

---
+++ HW3.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class hw3
+{
+
+   public static void main(String[] args) throws FileNotFoundException
+   {
+
+	File infile= new File("hw3input.txt");
+	int digit=0, word=0, upper=0, lower=0;
+	String line;
+
+	Scanner in =  new Scanner(infile);
+
+	Scanner keyboard = new Scanner(System.in);
+
+	PrintWriter out= new PrintWriter("hw3output.txt");
+
+	out.println("\t\t String Analysis Report");
+	out.println("\t\t =====================");
+
+	while(in.hasNextLine())
+	{
+
+       line = in.nextLine();
+
+       for(int i=0; i<line.length() ; i++)
+       {
+		   if(Character.isDigit(line.charAt(i)))
+		   digit++;
+		   else if(Character.isUpperCase(line.charAt(i)))
+		   		upper++;
+		   		else if (Character.isLowerCase(line.charAt(i)))
+		   			 lower++;
+		   			 else if(Character.isWhitespace(line.charAt(i)))
+		   				  word++;
+
+
+	   }
+
+	    out.println(line);
+	    out.println("Upper count =" + upper);
+	    out.println("Lower count =" +lower);
+	    out.println("digit count =" + digit);
+	    out.println("word count =" + (word +1));
+	    out.println("\n");
+
+	    digit=0;
+	    word=0;
+	    upper=0;
+	    lower=0;
+
+    }
+
+
+ 	in.close();
+ 	out.close();
+
+  }
+}
